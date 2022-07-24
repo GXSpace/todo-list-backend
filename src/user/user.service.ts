2c710d901bee4b0d77d81c5ffa3024b8342e2dd8
Gonçalo Abreu Corrêa Brito Da Silva <goncaloasilva@edu.ulisboa.pt>
2022-07-24 18:16:12 +0100

    feat(UserModule): Implemented CRUD api for Todo list

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { User } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { PrismaError } from 'prisma-error-enum';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { TodoDeleteDto, TodoDto, TodoPatchDto } from './ dto';
 
 @Injectable()
 export class UserService {
@@ -10,13 +13,68 @@ export class UserService {
     return user;
   }
 
-  getTodos(user: User) {
-    return this.prisma.user.findUnique({
+  async getTodos(user: User) {
+    return await this.prisma.user.findUnique({
       where: {
         id: user.id,
       },
       select: {
         todos: true,
+      },
+    });
+  }
+
+  async createTodo(user: User, todo: TodoDto) {
+    await this.prisma.todo.create({
+      data: {
+        authorId: user.id,
+        title: todo.title,
+        description: todo.description,
+        deadline: todo.deadline,
+      },
+    });
+  }
+
+  async editTodo(user: User, id: string, todo: TodoPatchDto) {
+    try {
+      await this.prisma.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          todos: {
+            update: {
+              where: {
+                id: Number(id),
+              },
+              data: todo,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === PrismaError.InterpretationError) {
+          throw new NotFoundException('This todo does not exist');
+        }
+      }
+    }
+  }
+
+  async deleteTodos(user: User, ids: TodoDeleteDto) {
+    const idsArray = [];
+    ids.ids.forEach((id) => {
+      idsArray.push({ id: id });
+    });
+
+    await this.prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        todos: {
+          deleteMany: idsArray,
+        },
       },
     });
   }
