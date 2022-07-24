be58c28d96f204bd6205cbf93ea6ad76b81d773d
Gonçalo Abreu Corrêa Brito Da Silva <goncaloasilva@edu.ulisboa.pt>
2022-07-24 03:46:22 +0100

    feat(AuthModule): Implemented JWT Authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto, NewUserDto } from './dto';
 
@@ -6,6 +6,7 @@ import { AuthDto, NewUserDto } from './dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @HttpCode(HttpStatus.OK)
   @Post('signin')
   signIn(@Body() dto: AuthDto) {
     return this.authService.signIn(dto);
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,10 +4,37 @@ import { AuthDto, NewUserDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { PrismaError } from 'prisma-error-enum';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email: email,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '5m',
+      secret: secret,
+    });
+
+    return {
+      access_token: token,
+    };
+  }
 
   async signIn(dto: AuthDto) {
     const user = await this.prisma.user.findUnique({
@@ -23,7 +50,7 @@ export class AuthService {
     if (!pswMatch) throw new ForbiddenException('Wrong password');
 
     delete user.password;
-    return user;
+    return this.signToken(user.id, user.email);
   }
 
   async signUp(dto: NewUserDto) {
@@ -39,8 +66,7 @@ export class AuthService {
       });
 
       delete user.password;
-
-      return user;
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === PrismaError.UniqueConstraintViolation) {
