9e8e24dd53ec4dcba18cb4b54a8d1a3ac5066b21
Gonçalo Abreu Corrêa Brito Da Silva <goncaloasilva@edu.ulisboa.pt>
2022-07-25 00:03:05 +0100

    feat(Unit Tests): Added Sign Up and Sign In endpoints

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,131 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { AppModule } from '../src/app.module';
+import { AuthDto, NewUserDto } from '../src/auth/dto';
 
-describe('AppController (e2e)', () => {
+describe('Todo App Backend e2e tests', () => {
   let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  let prisma: PrismaService;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
+    await app.listen(3333);
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3333');
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('Auth', () => {
+    const incompleteDto: any = {
+      email: 'test@gmail.com',
+    };
+
+    describe('Sign Up', () => {
+      const defaultDto: NewUserDto = {
+        email: 'test@gmail.com',
+        username: 'test',
+        password: '12345',
+      };
+
+      it('Should throw when missing some fields', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(incompleteDto)
+          .expectStatus(400);
+      });
+
+      it('Should sign up', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(defaultDto)
+          .expectStatus(201);
+      });
+
+      it('Should throw on duplicated account sign up', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(defaultDto)
+          .expectStatus(403);
+      });
+    });
+
+    describe('Sign In', () => {
+      const notexistsDto: AuthDto = {
+        email: 'notexists@gmail.com',
+        password: '12345',
+      };
+
+      const incorrectDto: AuthDto = {
+        email: 'test@gmail.com',
+        password: 'incorrect',
+      };
+
+      const correctDto: AuthDto = {
+        email: 'test@gmail.com',
+        password: '12345',
+      };
+
+      it('Should throw when missing some fields', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(incompleteDto)
+          .expectStatus(400);
+      });
+
+      it('Should throw when account not found', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(notexistsDto)
+          .expectStatus(403);
+      });
+
+      it('Should throw when incorrect password', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(incorrectDto)
+          .expectStatus(403);
+      });
+
+      it('Should sign in successfully', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(correctDto)
+          .expectStatus(200);
+      });
+    });
+  });
+
+  describe('User', () => {
+    describe('Get Profile', () => {});
+
+    describe('Todos', () => {
+      describe('Create Todo', () => {});
+      describe('Read Todos', () => {});
+      describe('Update Todo', () => {});
+      describe('Delete Todo', () => {});
+    });
+  });
+
+  afterAll(() => {
+    app.close();
   });
 });
